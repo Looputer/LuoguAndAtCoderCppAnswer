8d020e6c02c5de9a02455124dbbcbf1adf6f616a
looputer <luyichen_2013@163.com>
2026-02-01 17:14:32 +0800

    Cpp Code Commit

---
+++ B3611.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 陆熠辰 on 26-2-1.
+//
+#include <iostream>
+#define int long long
+using namespace std;
+
+int n;
+int dp[105][105];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> dp[i][j];
+        }
+    }
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] |= dp[i][k] & dp[k][j];
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
