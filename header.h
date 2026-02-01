91cba5f8cbd89d83b80ec7b67555d40b9351c47d
looputer <luyichen_2013@163.com>
2026-02-01 16:46:37 +0800

    Header Commit

---
+++ header.h
@@ -0,0 +1,14 @@
+//
+// Created by 陆熠辰 on 26-1-31.
+//
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <deque>
+#include <set>
+#include <map>
+#include <vector>
+#define int long long
