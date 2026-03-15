46b2ad68add3db1936896de9dbe9a066d13dd3ce
looputer <luyichen_2013@163.com>
2026-03-15 09:44:32 +0800

    Cpp Code Commit

---
+++ unknown_gesp20260301.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 陆熠辰 on 26-3-15.
+//
+#include <iostream>
+#define int long long
+using namespace std;
+
+const int MaxN = 1e5+5;
+int dp[MaxN], a[MaxN], b[MaxN];
+int n;
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i], dp[i] = a[i];
+    for (int i = 1; i <= n; i++) cin >> b[i];
+    // for (int i = 1; i <= n; i++) {
+    //     dp[i] = a[i];
+    //     for (int j = 1; j < i; j++) {
+    //         if (j + b[j] <= i)
+    //             dp[i] = max(dp[i], dp[j] + a[i]);
+    //     }
+    // }
+    for (int i = 1; i <= n; i++) {
+        if (i + b[i] <= n)
+            dp[i + b[i]] = max(dp[i + b[i]], dp[i] + a[i + b[i]]);
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++) ans = max(ans, dp[i]);
+    cout << dp[n] << endl;
+    return 0;
+}
---
+++ unknown_gesp20260302.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 陆熠辰 on 26-3-15.
+//
+#include <iostream>
+#include <vector>
+#define int long long
+using namespace std;
+
+struct State {
+    int h;
+    bool full, comp;
+};
+const int MaxN = 1e3+5;
+int l[MaxN], r[MaxN];
+int ans, n;
+
+State dfs(int u) {
+    if (u == 0) return {0, 1, 1};
+    auto L = dfs(l[u]);
+    auto R = dfs(r[u]);
+    int h = max(L.h, R.h) + 1;
+    bool full = L.full && R.full && (L.h == R.h);
+    bool comp = false;
+    if (L.comp && R.comp) {
+        if (L.h == R.h + 1 && R.full) comp = true;
+        else if (L.h == R.h && L.full) comp = true;
+    }
+    if (comp) ans++;
+    return {h, full, comp};
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> l[i] >> r[i];
+    }
+    dfs(1);
+    cout << ans;
+}
